f84275f0085211e1c97197d5c0f05ec939a91e9d
João Gomes <jowgomes3@gmail.com>
2024-08-21 18:16:47 -0300

    chore: add husky and lint staged setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,30 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import tsParser from "@typescript-eslint/parser";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  {
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
-      parser: tsParser,
-    },
-  },
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
-    settings: {
-      project: "./tsconfig.json",
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
     },
-  },
-  {
-    plugins: {
-      "@typescript-eslint": tseslint,
+    extends: ["plugin:react/recommended", "standard"],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
-  },
-  {
+    plugins: ["react"],
     rules: {
-      ...tseslint.configs.recommended.rules,
       "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
     },
   },
 ];
