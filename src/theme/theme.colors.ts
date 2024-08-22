83ca14c9b34c900245d4ecf5f61a73248cf77661
João Gomes <jowgomes3@gmail.com>
2024-08-22 14:21:28 -0300

    feat: add Theme Colors file

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,20 @@
+import { error } from "console";
+
+const Colors = {
+  primary: "#212529",
+  text: {
+    white: "#f8f9fa",
+    dark: "#343a40",
+  },
+  background: {
+    dark: "#212529",
+    white: "#f8f9fa",
+  },
+  input: {
+    background: "#e9ecef",
+    placeholder: "#6c857d",
+  },
+  error: "#FF6A6A",
+};
+
+export default Colors;
