e2cda6993f514ba6fbe4d966d58c0bb231e28d79
João Gomes <jowgomes3@gmail.com>
2024-08-22 11:28:16 -0300

    feat: add Categories Component

---
+++ src/components/categories/categories.component.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+// Utilities
+import Category from "../../types/category.types";
+
+// Styles
+import "./categories.styles.css";
+import env from "../../config/env.config";
+
+const Categories = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  const fetchCategories = async () => {
+    try {
+      const { data } = await axios.get(`${env.apiUrl}/api/category`);
+
+      console.log(data);
+      setCategories(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  return (
+    <div className="categories-container">
+      <div className="categories-content"></div>
+    </div>
+  );
+};
+
+export default Categories;
