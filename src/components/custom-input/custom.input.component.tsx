bc60d505fa90f4837f1841edf9d0abe6f5955bb5
João Gomes <jowgomes3@gmail.com>
2024-08-23 11:07:07 -0300

    feat: add ReactHookForm validation to LoginPage and CustomInput

--- src/components/custom-input/custom.input.component.tsx
+++ src/components/custom-input/custom.input.component.tsx
@@ -1,19 +1,18 @@
-import { FunctionComponent, InputHTMLAttributes } from "react";
+import React, { FunctionComponent, InputHTMLAttributes } from "react";
 
 // Styles
 import { CustomInputContainer } from "./custom.input.style";
 
 interface CustomInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  hasError?: boolean;
+  $hasError?: boolean;
 }
 
-const CustomInput: FunctionComponent<CustomInputProps> = ({
-  hasError,
-  ...rest
-}) => {
-  return (
-    <CustomInputContainer hasError={hasError} {...rest}></CustomInputContainer>
-  );
-};
+const CustomInput: FunctionComponent<CustomInputProps> = React.forwardRef(
+  (props, ref) => {
+    return <CustomInputContainer {...props} ref={ref as any} />;
+  },
+);
+
+CustomInput.displayName = "CustomInput";
 
 export default CustomInput;
