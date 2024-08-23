95550f10405ec56aff2e8a75352d18360f28c1e9
João Gomes <jowgomes3@gmail.com>
2024-08-23 15:41:06 -0300

    chore: add firebase configuration

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFD2e2c86iCcnIF4DD6cJSMqGgLnAehNs",
+  authDomain: "cosmo-ecommerce.firebaseapp.com",
+  projectId: "cosmo-ecommerce",
+  storageBucket: "cosmo-ecommerce.appspot.com",
+  messagingSenderId: "398549865590",
+  appId: "1:398549865590:web:ea6827dabd9e0651913b57",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
