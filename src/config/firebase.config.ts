4e7b766d01dec7170a95b19a79c32b25812ad876
João Gomes <jowgomes3@gmail.com>
2024-08-27 17:06:46 -0300

    feat: add login with Google

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCFD2e2c86iCcnIF4DD6cJSMqGgLnAehNs",
@@ -14,3 +14,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
