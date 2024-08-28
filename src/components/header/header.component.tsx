9d3c4e76c75efa4d3a47258f20bd0f3a4819ae04
João Gomes <jowgomes3@gmail.com>
2024-08-28 19:05:52 -0300

    feat: ensure login, account creation and signout button are shown based on authentication state

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,41 +1,58 @@
+import { signOut } from "firebase/auth";
 import { BsCart3 } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
 
 // Styles
 import {
   HeaderContainer,
-  HeaderItem,
   HeaderItems,
+  HeaderItem,
   HeaderTitle,
 } from "./header.styles";
-import { Link } from "react-router-dom";
-import Colors from "../../theme/theme.colors";
+
+// Utilities
+import { auth } from "../../config/firebase.config";
+import { UserContext } from "../../contexts/user.context";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const { isAuthenticated } = useContext(UserContext);
+
+  const handleLogoClick = () => {
+    navigate("/");
+  };
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  const handleSignUpClick = () => {
+    navigate("/sign-up");
+  };
+
   return (
     <HeaderContainer>
-      <Link
-        to="/"
-        style={{ textDecoration: "none", color: `${Colors.text.white} ` }}
-      >
-        <HeaderTitle>COSMO APPAREL</HeaderTitle>
-      </Link>
+      <HeaderTitle onClick={handleLogoClick}>CLUB CLOTHING</HeaderTitle>
 
       <HeaderItems>
         <HeaderItem>Explorar</HeaderItem>
-        <Link
-          to="/login"
-          style={{ textDecoration: "none", color: `${Colors.text.white} ` }}
-        >
-          <HeaderItem>Login</HeaderItem>
-        </Link>
-        <Link
-          to="/signup"
-          style={{ textDecoration: "none", color: `${Colors.text.white} ` }}
-        >
-          <HeaderItem>Criar conta</HeaderItem>
-        </Link>
+
+        {!isAuthenticated && (
+          <>
+            <HeaderItem onClick={handleLoginClick}>Login</HeaderItem>
+            <HeaderItem onClick={handleSignUpClick}>Criar Conta</HeaderItem>
+          </>
+        )}
+
+        {isAuthenticated && (
+          <HeaderItem onClick={() => signOut(auth)}>Sair</HeaderItem>
+        )}
+
         <HeaderItem>
-          <BsCart3 size={25} /> <p style={{ marginLeft: 5 }}> 5</p>
+          <BsCart3 size={25} />
+          <p style={{ marginLeft: 5 }}>5</p>
         </HeaderItem>
       </HeaderItems>
     </HeaderContainer>
