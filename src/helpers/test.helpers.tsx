0d42cdfaf5b6e8b265b43639c760a4fae522a79f
João Gomes <jowgomes3@gmail.com>
2024-09-06 17:36:48 -0300

    feat: add renderWithRedux helper function

---
+++ src/helpers/test.helpers.tsx
@@ -0,0 +1,32 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+
+import rootReducer from "../store/root-reducer";
+import { RootState } from "../store/store";
+
+export const renderWithRedux = (
+  component: React.ReactElement,
+  {
+    preloadedState,
+    store = configureStore({
+      reducer: rootReducer,
+      preloadedState,
+    }),
+    ...renderOptions
+  }: {
+    preloadedState: RootState;
+    store?: any;
+  },
+) => {
+  const Wrapper = ({ children }: { children: React.ReactElement }) => {
+    return (
+      <BrowserRouter>
+        <Provider store={store}>{children}</Provider>
+      </BrowserRouter>
+    );
+  };
+
+  return render(component, { wrapper: Wrapper, ...renderOptions });
+};
