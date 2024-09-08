666434463aa78fdb95a3061a25fda838b95fd9c5
João Gomes <jowgomes3@gmail.com>
2024-09-08 18:34:20 -0300

    test: ensure categories fetches and shows categories

---
+++ src/components/categories/categories.test.tsx
@@ -0,0 +1,38 @@
+import * as firestore from "firebase/firestore";
+import { renderWithRedux } from "../../helpers/test.helpers";
+import Categories from "./categories.component";
+
+jest.mock("firebase/firestore");
+
+jest.mock("firebase/auth", () => ({
+  getAuth: jest.fn(() => ({
+    currentUser: { uid: "123", email: "test@example.com" },
+  })),
+  GoogleAuthProvider: jest.fn(),
+  signOut: jest.fn(),
+}));
+
+describe("Categories", () => {
+  it("should fecth and show categories", async () => {
+    const mockedFirestore = firestore as any;
+    mockedFirestore.getDocs.mockReturnValue([
+      {
+        data() {
+          return {
+            id: "1",
+            displayName: "lorem ipsum",
+          };
+        },
+      },
+    ]);
+
+    mockedFirestore.collection.mockReturnValue({
+      withConverter: () => {},
+    });
+
+    const { getByText, findByText } = renderWithRedux(<Categories />, {});
+
+    await findByText("lorem ipsum");
+    getByText(/explorar/i);
+  });
+});
