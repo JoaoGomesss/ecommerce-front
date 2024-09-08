8ecf6b974f748130d07ab600b38e41bfa354804f
João Gomes <jowgomes3@gmail.com>
2024-09-08 20:21:26 -0300

    test: ensure sign up shows error if email is invalid

--- src/pages/sign-up/sign.up.test.tsx
+++ src/pages/sign-up/sign.up.test.tsx
@@ -12,10 +12,7 @@ jest.mock("firebase/auth", () => ({
 
 describe("Sign Up", () => {
   it("should show error when trying to submit without filling all required fields", async () => {
-    const { getByText, findByText, getAllByText } = renderWithRedux(
-      <SignUpPage />,
-      {},
-    );
+    const { getByText, findByText } = renderWithRedux(<SignUpPage />, {});
 
     const submitButton = getByText("Criar sua conta", { selector: "button" });
 
@@ -24,5 +21,22 @@ describe("Sign Up", () => {
     await findByText(/o nome é obrigatório/i);
     getByText(/o sobrenome é obrigatório/i);
     getByText(/o e-mail é obrigatório/i);
+  });
+
+  it("should show error when filling an invalid email", async () => {
+    const { getByText, findByText, getByPlaceholderText } = renderWithRedux(
+      <SignUpPage />,
+      {},
+    );
+
+    const emailInput = getByPlaceholderText(/digite seu e-mail/i);
+
+    userEvent.type(emailInput, "invalid_email");
+
+    const submitButton = getByText("Criar sua conta", { selector: "button" });
+
+    userEvent.click(submitButton);
+
+    await findByText(/insira um e-mail válido/i);
   });
 });
