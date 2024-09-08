a316c8d51111e499f06e962bfb93cfa6c50f7845
João Gomes <jowgomes3@gmail.com>
2024-09-08 20:03:37 -0300

    test: ensure login shows error if email or password is invalid

--- src/pages/login/login.test.tsx
+++ src/pages/login/login.test.tsx
@@ -1,13 +1,18 @@
 import userEvent from "@testing-library/user-event";
 import { renderWithRedux } from "../../helpers/test.helpers";
 import LoginPage from "./login.page";
+import * as firebaseAuth from "firebase/auth";
+import { AuthErrorCodes } from "firebase/auth";
 
 jest.mock("firebase/auth", () => ({
-  getAuth: jest.fn(() => ({
-    currentUser: { uid: "123", email: "test@example.com" },
-  })),
   GoogleAuthProvider: jest.fn(),
   signOut: jest.fn(),
+  getAuth: jest.fn(),
+  signInWithEmailAndPassword: jest.fn(),
+  AuthErrorCodes: {
+    INVALID_PASSWORD: "auth/wrong-password",
+    USER_DELETED: "auth/user-not-found",
+  },
 }));
 
 describe("Login", () => {
@@ -37,5 +42,59 @@ describe("Login", () => {
     userEvent.click(submitButton);
 
     await findByText(/insira um e-mail válido!/i);
+  });
+
+  it("should show an error if email is not found", async () => {
+    const mockFirebaseAuth = firebaseAuth as any;
+
+    mockFirebaseAuth.signInWithEmailAndPassword.mockImplementation(() =>
+      Promise.reject({ code: AuthErrorCodes.USER_DELETED }),
+    );
+
+    const { getByPlaceholderText, getByText, findByText } = renderWithRedux(
+      <LoginPage />,
+      {},
+    );
+
+    const emailInput = getByPlaceholderText(/digite seu e-mail/i);
+
+    userEvent.type(emailInput, "lorem@ipsum.com");
+
+    const passwordIput = getByPlaceholderText(/digite sua senha/i);
+
+    userEvent.type(passwordIput, "234566");
+
+    const submitButton = getByText("Entrar");
+
+    userEvent.click(submitButton);
+
+    await findByText(/não existe um usuário com esse email/i);
+  });
+
+  it("should show an error if password is not found", async () => {
+    const mockFirebaseAuth = firebaseAuth as any;
+
+    mockFirebaseAuth.signInWithEmailAndPassword.mockImplementation(() =>
+      Promise.reject({ code: AuthErrorCodes.INVALID_PASSWORD }),
+    );
+
+    const { getByPlaceholderText, getByText, findByText } = renderWithRedux(
+      <LoginPage />,
+      {},
+    );
+
+    const emailInput = getByPlaceholderText(/digite seu e-mail/i);
+
+    userEvent.type(emailInput, "lorem@ipsum.com");
+
+    const passwordIput = getByPlaceholderText(/digite sua senha/i);
+
+    userEvent.type(passwordIput, "123456");
+
+    const submitButton = getByText("Entrar");
+
+    userEvent.click(submitButton);
+
+    await findByText(/senha inválida/i);
   });
 });
